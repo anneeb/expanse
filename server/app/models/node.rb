8f537b33ee7ba27421fd449886c5105e3ddb5203
anneeb <ahuang852@gmail.com>
2017-06-20 13:03:00 -0400

    add validations

--- server/app/models/node.rb
+++ server/app/models/node.rb
@@ -2,5 +2,7 @@ class Node < ApplicationRecord
   belongs_to :space
   has_many :children, class_name: "Node", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Node", optional: true
+  validates :title, presence: true
+  validates :body, presence: true
 
 end
--- server/app/models/space.rb
+++ server/app/models/space.rb
@@ -1,3 +1,5 @@
 class Space < ApplicationRecord
   has_many :nodes
+  validates :title, presence: true
+  validates :creator, presence: true
 end
