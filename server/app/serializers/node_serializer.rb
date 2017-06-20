e062fb4578dcb6b88f0c4afacf45de6ce2099ebb
anneeb <ahuang852@gmail.com>
2017-06-20 14:05:40 -0400

    dependent destroy

--- server/app/serializers/node_serializer.rb
+++ server/app/serializers/node_serializer.rb
@@ -1,5 +1,5 @@
 class NodeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body
+  attributes :id, :title, :body, :space_id, :parent_id
   belongs_to :space
   belongs_to :parent, class_name: "Node"
   has_many :children, class_name: "Node", foreign_key: "parent_id"
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -12,13 +12,36 @@ godnode = Node.new(title:"God Node", body: "I have no parent")
 
 c1 = Node.new(title: "Child Node 1", body: "I am a child")
 c2 = Node.new(title: "Child Node 2", body: "I am also a child")
+c3 = Node.new(title: "Child Node 3", body: "I am a child 3")
+c4 = Node.new(title: "Child Node 4", body: "I am a child 4")
 
 space.nodes << godnode
 
-space.nodes << c1
+space.nodes.push(c1, c2, c3, c4)
 
-space.nodes << c2
+godnode.children.push(c1, c2)
 
-godnode.children << c1
+c1.children << c3
 
-godnode.children << c2
+c2.children << c4
+
+
+
+space = Space.create(title:"My Cool Space 2", creator:"Annee 2")
+
+godnode = Node.new(title:"God Node 2", body: "I have no parent 2")
+
+c1 = Node.new(title: "Child Node 1 - 2", body: "I am a child - 2")
+c2 = Node.new(title: "Child Node 2 - 2", body: "I am also a child - 2")
+c3 = Node.new(title: "Child Node 3 - 2", body: "I am a child 3 - 2")
+c4 = Node.new(title: "Child Node 4 - 2", body: "I am a child 4 - 2")
+
+space.nodes << godnode
+
+space.nodes.push(c1, c2, c3, c4)
+
+godnode.children.push(c1, c2)
+
+c1.children << c3
+
+c2.children << c4
