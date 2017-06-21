3d0087da968f2f3a839a2f9b6503e49adb63aee8
nkernis <nkernis@gmail.com>
2017-06-20 23:06:10 -0400

    boxes and lines now center based off canvas size

--- client/src/Node.js
+++ client/src/Node.js
@@ -15,18 +15,17 @@ class Node {
     square.graphics.setStrokeStyle(1).beginStroke("rgba(0,0,0,1)").drawRect(0, 0, 50, 50)
     container.x = array[0][0]
     container.y = array[0][1]
-    container.addChild(square, label);
+    container.addChild(square, label)
     this.container = container
   }
 
   drawLine(array) {
     // array format: [[x, y, w, h], [startX, startY, endX, endY]]
-    let line = new createjs.Shape();
-    line.graphics.setStrokeStyle(1);
-    line.graphics.beginStroke("#000000");
-    line.graphics.moveTo(array[1][0], array[1][1]);
-    line.graphics.lineTo(array[1][2], array[1][3]);
-    line.graphics.endStroke();
+    let line = new createjs.Shape()
+    line.graphics.setStrokeStyle(1)
+    line.graphics.beginStroke("#000000")
+    line.graphics.moveTo(array[1][0], array[1][1])
+    line.graphics.lineTo(array[1][2], array[1][3])
     this.line = line
   }
 
