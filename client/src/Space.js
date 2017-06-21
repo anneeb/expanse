9119fea9e04e94b3c52284e510d101ec8504529b
nkernis <nkernis@gmail.com>
2017-06-21 09:48:00 -0400

    checks for what was clicked

--- client/src/Space.js
+++ client/src/Space.js
@@ -4,12 +4,7 @@ class Space {
     this.nextNode = 0
     this.stage = new createjs.Stage("space")
     this.stage.enableDOMEvents(true)
-    this.stage.on("click", function(evt) {
-    console.log(`${evt.target}`)
-});
-
-
-
+    this.stage.on("click", this.test.bind(this))
     this.boxChangeLeft = 100
     this.boxChangeRight = 100
     this.lineChangeLeft = 75
@@ -26,20 +21,25 @@ class Space {
         return [[this.stage.canvas.width / 2, 5, 50, 50], [0]]
     } else if (this.nextNode === 1) {
         return [[this.stage.canvas.width / 2, 100, 50, 50], [(this.stage.canvas.width / 2) + 25, 55, (this.stage.canvas.width / 2) + 25, 100]]
-
-
     } else if (this.nextNode % 2 === 0) {
         let x = [[(this.stage.canvas.width / 2) - this.boxChangeLeft, 100, 50, 50], [(this.stage.canvas.width / 2) + 25, 55, (this.stage.canvas.width / 2) - this.lineChangeLeft, 100]]
         this.boxChangeLeft += 100
         this.lineChangeLeft += 100
         return x
-
       } else if (this.nextNode % 2 !== 0) {
         let x = [[(this.stage.canvas.width / 2) + this.boxChangeRight, 100, 50, 50], [(this.stage.canvas.width / 2) + 25, 55, (this.stage.canvas.width / 2) + this.lineChangeRight, 100]]
         this.boxChangeRight += 100
         this.lineChangeRight += 100
         return x
       }
+  }
+
+  test(event) {
+    for (let i = 0; i < this.nodeList.length; i++) {
+      if (this.nodeList[i].container.children[0].name === event.target.name) {
+        console.log(`What Should I Do With: ${this.nodeList[i].container.children[0].name}`);
+      }
+    }
   }
 
   renderSpace() {
