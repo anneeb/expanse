7d0d49f3b5e625b0d1e860ed917f547fe2fe1a53
nkernis <nkernis@gmail.com>
2017-06-24 17:55:17 -0400

    text shows up, dynamically sets size, but position is off

--- client/src/node.js
+++ client/src/node.js
@@ -40,7 +40,50 @@ class Node {
     this.space.three.scene.add(node);
     this.space.three.nodes.push(node);
 
+    // Test Sprite
+    var spritey = this.makeTextSprite(""+ this.title  + "", arr[2])
+  	spritey.position.set(arr[0][0],arr[0][1],arr[0][2])
+  	this.space.three.scene.add( spritey )
+
     // array format: [[nx, ny, nz], [px, py, pz], g]
+  }
+
+
+  makeTextSprite(message, fontSize){
+    console.log(fontSize);
+
+  	var fontface = "Arial";
+  	var fontsize = fontSize
+  	var borderThickness = 4;
+  	var borderColor =  {r:100, g:100, b:255, a:1};
+  	var backgroundColor = {r:255, g:100, b:100, a:1};
+    // vvv Is desprected vvv
+  	// var spriteAlignment = THREE.SpriteAlignment.topLeft;
+
+  	var spriteAlignment = new THREE.Vector2( 1, -1 );
+
+  	var canvas = document.createElement('canvas');
+  	var context = canvas.getContext('2d');
+  	context.font = "Bold " + fontsize + "px " + fontface;
+
+  	// get size data (height depends only on font size)
+  	var metrics = context.measureText( message );
+  	var textWidth = metrics.width;
+
+  	// text color
+  	context.fillStyle = "rgba(255, 0, 0, 1)";
+
+  	context.fillText( message, borderThickness, fontsize + borderThickness);
+
+  	// canvas contents will be used for a texture
+  	var texture = new THREE.Texture(canvas)
+  	texture.needsUpdate = true;
+
+  	var spriteMaterial = new THREE.SpriteMaterial(
+  		{ map: texture, useScreenCoordinates: false, alignment: spriteAlignment } );
+  	var sprite = new THREE.Sprite( spriteMaterial );
+  	sprite.scale.set(100,50,1.0);
+  	return sprite;
   }
 
   drawLine(arr) {
